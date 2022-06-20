263c41ad61b64e24b3a8f86af9e6b684254eb984
Abdiel Ramirez <arts.devweb@gmail.com>
2022-06-20 16:30:54 +0200

    integrate workflow for augmented plus original images

--- cheetah/augment/data.py
+++ cheetah/augment/data.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from cheetah.params import BUCKET_AUGMENT_DATA_PATH, BUCKET_NAME
+from cheetah.params import BUCKET_AUGMENT_DATA_PATH, BUCKET_NAME, BUCKET_TEST_DATA_PATH
 import os
 from google.cloud import storage
 from glob import glob
@@ -10,11 +10,19 @@ ENV = os.environ.get('ENV', default="gcp")
 
 
 def get_augment_data():
-    """method to get the training data (or a portion of it) from google cloud bucket"""
+    """Gets the augmented data and the the test metadata prepared with
+    cheetah.augment._generate_test_csv().
+
+    Returns two pd.Dataframes for augmented and test images.
+    """
     path = f"gs://{BUCKET_NAME}/{BUCKET_AUGMENT_DATA_PATH}" #gcp path by default
+    test_path = f"gs://{BUCKET_NAME}/{BUCKET_TEST_DATA_PATH}"
     if ENV == 'local':
         path = 'raw_data/augment/mel_augmented.csv'
-    return pd.read_csv(path)
+        test_path = 'raw_data/augment/mel_test.csv'
+    augment_df = pd.read_csv(path)
+    test_mel_df = pd.read_csv(test_path)
+    return augment_df, test_mel_df
 
 def path_to_metadata(df):
     '''Adds a column to the dataframe pointing to the path of the image on the
@@ -40,21 +48,41 @@ def path_to_metadata(df):
     df['path'] = df['image_id'].map(imageid_path_dict.get)
     return df
 
-def data_augment_balancer(df, aug_df, n_images=1113):
-    '''Returns a metadata set with equal number of melanoma and nevi observations'''
-    # Retrieve Nevi
+def data_augment_balancer(df, aug_df, class_size=2000):
+    '''Prepare a dataframe with balanced number of classes of set_size
+    number of images with balance ratio of non_mel to mel. The balance
+    takes (set_size/2 - n) augmented images, where n is the number of
+    original images.
+
+    Args:
+        df: pd.Dataframe with original data.
+        aug_df: pd.Dataframe with augmented data.
+        set_size: Size of the final dataset
+
+    Returns:
+        balanced_meta: pd.Dataframe with balanced images via data augmentation.
+    '''
+    # Retrieve Nevi and Melanoma from original dataset
     rest_df = df.query('dx == "nv"')
+    mel_df = df.query('dx == "mel"')
+
     #randomly takes indexes equal number of melanoma observations
-    idx_nonmel = np.random.choice(list(rest_df.index), n_images)
-    non_mel_balanced = df.iloc[idx_nonmel]
-    # add path to non_mel df
-    non_mel_balanced = path_to_metadata(non_mel_balanced)
+    train_test_rest = rest_df.sample(0.1 * len(rest_df) + class_size)
+
+    # add path to non_melanoma df
+    train_test_rest = path_to_metadata(train_test_rest)
+    # holdout
+    train_rest = train_test_rest[:class_size]
+    test_rest = train_test_rest[class_size:]
+
+    # add path to melanoma df
+    mel_df = path_to_metadata(mel_df)
 
     # Retrieve Melanoma augmented images
-    idx_mel = np.random.choice(list(aug_df.index), n_images)
+    idx_mel = np.random.choice(list(aug_df.index), class_size - len(mel_df))
     mel_augmented = df.iloc[idx_mel]
 
     print('********** Taking augmented data **********')
-    balanced_meta = pd.concat([non_mel_balanced,mel_augmented], axis=0)
+    balanced_meta = pd.concat([train_rest,mel_df,mel_augmented], axis=0)
 
-    return balanced_meta
+    return balanced_meta, test_rest
--- cheetah/augment/trainer.py
+++ cheetah/augment/trainer.py
@@ -7,6 +7,7 @@ from cheetah.utils import configure_for_performance
 from cheetah.model import initialize_model, compile, fit_with_earlystop
 import os
 from datetime import datetime
+import pandas as pd
 
 class Trainer(MLFlowBase):
 
@@ -23,20 +24,22 @@ class Trainer(MLFlowBase):
         # load data and get corresponding image paths
         df = data.get_data()
         df = data.path_to_metadata(df)
-        aug_df = get_augment_data()
+        # load augmented images and test separated
+        aug_df, test_mel_df = get_augment_data()
 
-        # balance data mel-augmented images vs non-mel
-        df = data_augment_balancer(df, aug_df, n_images=1113)
+        # balance melanoma + augmented_melanoma images vs non-mel
+        df, test_nonmel_df = data_augment_balancer(df, aug_df, class_size=2226)
 
-        # prepare tensor dataframe with extracted images and labels
-        ds = prepare_dataset(df)
+        # prepare train_val and test dataset with extracted images and labels
+        train_val_ds = prepare_dataset(df)
+        test_df = pd.concat([test_nonmel_df,test_mel_df], axis=0)
+        test = prepare_dataset(test_df)
 
-        # shuffle the dataset
-        ds = ds.shuffle(len(ds), reshuffle_each_iteration=False)
+        # shuffle the train_val dataset
+        train_val_ds = train_val_ds.shuffle(len(train_val_ds), reshuffle_each_iteration=False)
 
         # train validation holdout
-        train_val, test = tf_train_val_split(ds,val_ratio=0.1)
-        train, val = tf_train_val_split(train_val,val_ratio=0.2)
+        train, val = tf_train_val_split(train_val_ds,val_ratio=0.2)
 
         # optimize pipeline performance
         train = configure_for_performance(train)
@@ -48,7 +51,7 @@ class Trainer(MLFlowBase):
         # compile
         model = compile(model)
         # train model and get duration via timer_func decorator
-        model, duration = fit_with_earlystop(model, train, val, patience=20)
+        model, duration = fit_with_earlystop(model, train, val, patience=10)
 
         # evaluate model and get loss and accuracy
         loss, acc = model.evaluate(test)
@@ -67,7 +70,8 @@ class Trainer(MLFlowBase):
 
         self.mlflow_log_param("model_name", MODEL_NAME)
         self.mlflow_log_param("batch_size",BATCH_SIZE)
-        self.mlflow_log_param("n_images", ds.cardinality().numpy())
+        self.mlflow_log_param("n_images", train_val_ds.cardinality().numpy() \
+                                            + test.cardinality().numpy())
         self.mlflow_log_param("image_size",(IMAGE_HEIGHT,IMAGE_WIDTH))
         self.mlflow_log_param("n_params", f'{model.count_params():,}')
         self.mlflow_log_param("duration", f'{duration:.2f}')
